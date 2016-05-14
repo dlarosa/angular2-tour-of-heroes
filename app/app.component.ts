c0ca98278808f297348e2b35c2c12e5ccba67a19
dlarosa <mdelares@outlook.com>
2016-05-14 16:10:54 -0500

    part 6 - routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,89 +1,48 @@
-import {Component} from '@angular/core';
-import { Hero } from './hero';
+import { Component }       from '@angular/core';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService } from './hero.service';
-import { OnInit } from '@angular/core';
 
-
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
+])
 
 @Component({
-    selector: 'my-app',
-    template: `
+  selector: 'my-app',
+  template: `
     <h1>{{title}}</h1>
-    <ul class="heroes">
-      <li *ngFor="let hero of heroes"
-      [class.selected]="hero === selectedHero"
-       (click)="onSelect(hero)">
-         <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
-    </ul>
-    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-    
-    `,
-    styles:[`
-      .selected {
-        background-color: #CFD8DC !important;
-        color: white;
-      }
-      .heroes {
-        margin: 0 0 2em 0;
-        list-style-type: none;
-        padding: 0;
-        width: 15em;
-      }
-      .heroes li {
-        cursor: pointer;
-        position: relative;
-        left: 0;
-        background-color: #EEE;
-        margin: .5em;
-        padding: .3em 0;
-        height: 1.6em;
-        border-radius: 4px;
-      }
-      .heroes li.selected:hover {
-        background-color: #BBD8DC !important;
-        color: white;
-      }
-      .heroes li:hover {
-        color: #607D8B;
-        background-color: #DDD;
-        left: .1em;
-      }
-      .heroes .text {
-        position: relative;
-        top: -3px;
-      }
-      .heroes .badge {
-        display: inline-block;
-        font-size: small;
-        color: white;
-        padding: 0.8em 0.7em 0 0.7em;
-        background-color: #607D8B;
-        line-height: 1em;
-        position: relative;
-        left: -1px;
-        top: -4px;
-        height: 1.8em;
-        margin-right: .8em;
-        border-radius: 4px 0 0 4px;
-      }
-`],
-directives: [HeroDetailComponent],
-providers: [HeroService]
+    <nav>
+        <a [routerLink]="['Dashboard']">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app/app.component.css'],
+  directives: [
+      ROUTER_DIRECTIVES
+  ],
+  providers: [
+    HeroService,
+    ROUTER_PROVIDERS
+  ],
 })
-export class AppComponent implements OnInit {
-  ngOnInit() {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-  constructor(private heroService: HeroService) { 
-    
-  }
-  selectedHero: Hero;
-  onSelect(hero: Hero) { this.selectedHero = hero; }
-  public heroes: Hero[];
+export class AppComponent {
   title = 'Tour of Heroes';
 }
-
-
-
---
+++ app/dashboard.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
+import { Hero} from './hero';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router-deprecated';
+
+@Component({
+  selector: 'my-dashboard',
+  templateUrl: 'app/dashboard.component.html',
+  styleUrls: ['app/dashboard.component.css']
+})
+
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+  constructor(private router: Router, private heroService: HeroService) {}
+  ngOnInit() {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1,5));
+  }
+  gotoDetail(hero: Hero) {
+    let link = ['HeroDetail', { id: hero.id }];
+    this.router.navigate(link);
+  }
+}
